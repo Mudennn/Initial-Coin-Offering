4148588c5948431a7e812e9cf9092502b93b6f6f
Muden <mudennn@yahoo.com>
2022-09-12 09:02:17 +0800

    change hardcode key

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -9,8 +9,8 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/TXSFNibxlR5u_KEwvNMTtNxBLgBlXrZh",
-      accounts: ["bf2ee9b93e957e076d8d9280d7962126a33d9c040ab32ec8c1027015db4cee8e"],
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
 };
